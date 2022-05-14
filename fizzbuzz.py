6d6118b01e07d89a07cc2d0ffcc95dd8d3bf936a
Dylan Thinnes <dylan.thinnes@protonmail.com>
2022-05-14 20:38:29 +0100

    Amend python impl to be a generator

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 import sys
 
-for inp in sys.stdin:
-    i = int(inp)
-    out = ""
-    if i % 3 == 0:
-        out += "Fizz"
-    if i % 5 == 0:
-        out += "Buzz"
-    if i % 7 == 0:
-        out += "Quux"
-    if len(out) > 0:
-        print(out)
-    else:
-        print(i)
+def fizzbuzz():
+    for inp in sys.stdin:
+        i = int(inp)
+
+        out = ""
+        if i % 3 == 0:
+            out += "Fizz"
+        if i % 5 == 0:
+            out += "Buzz"
+        if i % 7 == 0:
+            out += "Quux"
+
+        if len(out) > 0:
+            yield out
+        else:
+            yield i
+
+for word in fizzbuzz():
+    print(word)
